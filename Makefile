de609fbefd46894368aa532888259e5a5e009cdb
Paul Ribolzi <pribolzi@k1r2p3.42mulhouse.fr>
2025-01-15 16:32:36 +0100

    printf

---
+++ Makefile
@@ -0,0 +1,27 @@
+NAME = libftprintf.a
+
+SRC = ft_printf.c ft_printfutils.c ft_printhexa.c
+
+OBJS = $(SRC:.c=.o)
+
+FLAGS = -Wall -Wextra -Werror
+
+CC = cc
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	ar rc $(NAME) $(OBJS)
+
+$(OBJS): $(SRC)
+	$(CC) $(FLAGS) -c $(SRC)
+
+clean:
+	rm -rf $(OBJS) $(OBJS_BONUS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ mainprintf.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainprintf.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pribolzi <pribolzi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 17:28:39 by pribolzi          #+#    #+#             */
+/*   Updated: 2024/11/19 18:08:23 by pribolzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdio.h>
+
+int main()
+{
+	void *ptr;
+	int a = 500;
+
+	ptr = &a;	
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test char : %c\n", 'c'));
+	printf("Valeur printf : %d\n\n", printf("Test char : %c\n", 'c'));
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test string : %s\n", "Phrase"));
+	printf("Valeur printf : %d\n\n", printf("Test string : %s\n", "Phrase"));
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test ptr : %p\n", ptr));
+	printf("Valeur printf : %d\n\n", printf("Test ptr : %p\n", ptr));
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test decimal : %d\n", 0));
+	printf("Valeur printf : %d\n\n", printf("Test decimal : %d\n", 0));
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test int : %i\n", 2147483647));
+	printf("Valeur printf : %d\n\n", printf("Test int : %i\n", 2147483647));
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test unsigned decimal : %u\n", 100));
+	printf("Valeur printf : %d\n\n", printf("Test unsigned decimal : %u\n", 100));
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test hexa : %x\n", 100));
+	printf("Valeur printf : %d\n\n", printf("Test hexa : %x\n", 100));
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test hexa upper : %X\n", 100));
+	printf("Valeur printf : %d\n\n", printf("Test hexa upper : %X\n", 100));
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test char : %%\n"));
+	printf("Valeur printf : %d\n\n", printf("Test char : %%\n"));
+	ft_printf("Valeur ft_printf : %d\n", ft_printf("Test de tout les flags : %c, %s, %p, %d, %i, %u, %x, %X, %%\n", 'c', "phrase", ptr, 500, 500, 100, 100, 100));
+	printf("Valeur printf : %d\n", printf("Test de tout les flags : %c, %s, %p, %d, %i, %u, %x, %X, %%\n", 'c', "phrase", ptr, 500, 500, 100, 100, 100));
+}
